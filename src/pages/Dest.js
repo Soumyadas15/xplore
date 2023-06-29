d3b74d962fea30c448097b8a12f7d29d44745a4e
Saumyapratim Das <saumyadas2017@gmail.com>
2023-06-30 00:56:59 +0530

    New fix

--- src/pages/Dest.js
+++ src/pages/Dest.js
@@ -4,11 +4,11 @@ import '../styles/Dest.css';
 export default function Dest() {
   return (
     <>
+
     <div className="about-us">
       <h1 className="about-us-heading">Destinations</h1>
       <p className="about-us-text">
-      
-
+    
       </p>
     </div>
     </>
