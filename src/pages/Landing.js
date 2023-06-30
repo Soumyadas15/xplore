7ca4ac47c8b22721ceb4f595434ffde066ef978e
Saumyapratim Das <saumyadas2017@gmail.com>
2023-06-30 13:05:42 +0530

    Landing page bug fix

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -12,12 +12,12 @@ function Landing() {
           <source src={video} type='video/mp4' />
         </video>
         <div className='landing-container'>
-          <h1>Welcome to XPLORE <i className='fas fa-plane'></i></h1>
-          <div>Explore the local areas</div>
+          <h1 className='animated-text'>Welcome to <span className='name'>Xplore</span> <i className='fas fa-plane'></i></h1>
+          <div className='animated-text'>Explore the local areas</div>
         </div>
       </div>
-      <Carousel/>
-      <Dest/>
+      <Carousel />
+      <Dest />
     </>
   );
 }
