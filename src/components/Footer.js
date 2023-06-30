1b7ebefd2653c6b152017ec12a4e0a0bf3efeae6
Saumyapratim Das <saumyadas2017@gmail.com>
2023-06-30 13:26:02 +0530

    Created a footer component and added it to App.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+export default function Footer() {
+  return (
+    <div className='footer-container'>
+        <h1>Created by <span>Soumya</span></h1>
+        <p>Front end project created using React.js</p>
+    </div>
+  )
+}
