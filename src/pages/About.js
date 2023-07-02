e506fc24b9457b97096a12ff063ab81d4124ef67
Souzumaki <sougatakansabanik02@gmail.com>
2023-07-02 18:16:40 +0530

    Just created the about page, will work on it later

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,33 +1,53 @@
-import React from 'react'
-import Carousel from '../components/Carousel'
+import React from 'react';
+import Carousel from '../components/Carousel';
+import '../styles/About.css';
 
-export default function About() {
+const About = () => {
   return (
-    <>
-    <div className="about-us">
-      <h1 className="about-us-heading">Why xplore?</h1>
-      <p className="about-us-text">
-      Travelling is not only about exploring the famous places. 
-      It's about delving deep into a culture, immersing oneself 
-      in its customs and traditions. 
-      
-      Xplore is your companion in finding the lesser-known gems 
-      that truly capture the essence of your holiday destination. 
-      Xplore goes beyond the usual tourist attractions, guiding 
-      you towards hidden treasures that embody the local way of life. 
-      From vibrant street markets and traditional festivals to cozy 
-      cafes and breathtaking natural wonders, Xplore ensures an authentic 
-      and immersive experience. Discovering these hidden gems 
-      allows you to forge meaningful connections with the local 
-      community, fostering cultural appreciation and leaving a 
-      lasting impact. 
-      
-      With Xplore, your journey becomes a profound exploration 
-      of a culture from within, creating cherished memories that 
-      go beyond surface-level tourism.
-
+    <div className="about-container">
+      <h1>About Xplore</h1>
+      <p>
+        Welcome to Xplore, your ultimate destination for unforgettable travel experiences. We are passionate about creating incredible journeys that connect you with the world's most captivating destinations.
       </p>
+
+      <Carousel />
+
+      <div className="mission-container">
+        <h2>Our Mission</h2>
+        <p>
+          At Xplore, our mission is to inspire and enable people to explore the world. We strive to provide exceptional travel services that exceed our customers' expectations and create lifelong memories.
+        </p>
+      </div>
+
+      <div className="vision-container">
+        <h2>Our Vision</h2>
+        <p>
+          We envision a world where everyone has the opportunity to discover and appreciate the beauty and diversity of our planet. Through our carefully crafted itineraries and dedicated team, we aim to make travel accessible, enjoyable, and sustainable for all.
+        </p>
+      </div>
+
+      <div className="team-container">
+        <h2>Meet Our Team</h2>
+        <p>
+          Behind Xplore is a team of experienced travel enthusiasts who are dedicated to curating unique and enriching travel experiences. Our team consists of expert guides, travel planners, and customer support professionals, all working together to ensure your journey is seamless and memorable.
+        </p>
+      </div>
+
+      <div className="testimonials-container">
+        <h2>What Our Customers Say</h2>
+        {/* Add testimonials or customer reviews here */}
+      </div>
+
+      <div className="contact-container">
+        <h2>Contact Us</h2>
+        <p>
+          If you have any questions, feedback, or would like to start planning your next adventure, please don't hesitate to get in touch with us. Our friendly customer support team is available 24/7 to assist you.
+        </p>
+        {/* Add contact form or contact information here */}
+      </div>
     </div>
-    </>
-  )
-}
+  );
+};
+
+export default About;
+
