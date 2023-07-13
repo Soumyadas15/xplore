64416b442c5978a538d922860758b6fdbab80696
Suman Maji <sumanmaji349@gmail.com>
2023-07-14 00:25:10 +0530

    Info section w/o icons

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,9 +5,18 @@ const Info = () => {
   return (
     <>
         <div className='infoEx'>
-            <h1>
-                Info section
-            </h1>
+            <div className='customer'>
+                <span className='upper'>10M+</span>
+                <span className='lower'>Happy customers</span>
+            </div>
+            <div className='hotel'>
+                <span className='upper'>500+</span>
+                <span className='lower'>Hotels in top locations</span>
+            </div>
+            <div className='support'>
+                <span className='upper'>24x7</span>
+                <span className='lower'>Customer support</span>
+            </div>
         </div>
     </>
   )
