5da15b11c7ca687785653a9c7b0dc63103ddcbaf
Suman Maji <sumanmaji349@gmail.com>
2023-07-13 23:10:01 +0530

    Testing

---
+++ src/components/Info.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/Info.css'
+
+const Info = () => {
+  return (
+    <>
+        <div className='infoEx'>
+            <h1>
+                Info section
+            </h1>
+        </div>
+    </>
+  )
+}
+
+export default Info
